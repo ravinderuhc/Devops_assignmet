8e55f329245438057f23207d99fc7f55b9d44021
Ravinder Kumar <ravinde.uhc@gmail.com>
2024-03-05 22:16:50 +0530

    Fixing Vulernabliets

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
+app.config['WTF_CSRF_ENABLED'] = False # Sensitive
 
 # Dummy data to simulate tasks
 tasks = [
